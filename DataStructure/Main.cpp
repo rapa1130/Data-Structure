5661e8ca5d30ca0ec1733101c1a2d3404b5a452b
rapa1130 <49236828+rapa1130@users.noreply.github.com>
2024-12-31 09:36:54 +0900

    Chore: Organize Vector Polynomial

--- DataStructure/Main.cpp
+++ DataStructure/Main.cpp
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-#include<iostream>
-=======
-#include"VectorPolynomial.h"
->>>>>>> 8b560217051041b4a57b9cd669cfab64d6453ed3
+#include"Ch2/VectorPolynomial.h"
 using namespace std;
-<<<<<<< HEAD
-
-int main(){
-    cout<<"a";
-=======
 int main() {
 	VectorPolynomial pol1;
 	pol1.NewTerm(Term(1, 3));
 	pol1.NewTerm(Term(2, 3));
 	pol1.NewTerm(Term(4, 3));
 	cout << pol1;
->>>>>>> 8b560217051041b4a57b9cd669cfab64d6453ed3
 }
