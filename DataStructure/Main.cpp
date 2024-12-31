7e4926ef5e14aa7baec59a769b096ea2e8825048
rapa1130 <49236828+rapa1130@users.noreply.github.com>
2024-12-31 09:33:56 +0900

    Main Conflict

--- DataStructure/Main.cpp
+++ DataStructure/Main.cpp
@@ -1,9 +1,19 @@
+<<<<<<< HEAD
+#include<iostream>
+=======
 #include"VectorPolynomial.h"
+>>>>>>> 8b560217051041b4a57b9cd669cfab64d6453ed3
 using namespace std;
+<<<<<<< HEAD
+
+int main(){
+    cout<<"a";
+=======
 int main() {
 	VectorPolynomial pol1;
 	pol1.NewTerm(Term(1, 3));
 	pol1.NewTerm(Term(2, 3));
 	pol1.NewTerm(Term(4, 3));
 	cout << pol1;
+>>>>>>> 8b560217051041b4a57b9cd669cfab64d6453ed3
 }
